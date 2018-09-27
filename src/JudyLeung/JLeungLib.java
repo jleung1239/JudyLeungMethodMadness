c5663a99447833e494699afd387fe6021aab9572
BT_1N3_11@BKTECH.LOCAL <BT_1N3_11@BKTECH.LOCAL>
2018-09-27 08:44:00 -0400

    Initial commit

---
+++ src/JudyLeung/JLeungLib.java
@@ -0,0 +1,10 @@
+package JudyLeung;
+// create methods here
+public class JLeungLib {
+    public static String test(String input)
+    {
+        String out = "Hello";
+        System.out.println(out + "There");
+    }
+
+}
---
+++ src/JudyLeung/Main.java
@@ -0,0 +1,9 @@
+package JudyLeung;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(+JLeungLib.test("hi"));
+    }
+}
