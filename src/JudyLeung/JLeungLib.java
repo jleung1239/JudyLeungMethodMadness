db3e4052b8747ecdd8fbb3ebd4486faaf0824978
jleung1239 <jleung1239@bths.edu>
2018-10-02 08:23:45 -0400

    testing

--- src/JudyLeung/JLeungLib.java
+++ src/JudyLeung/JLeungLib.java
@@ -6,6 +6,7 @@ public class JLeungLib {
         String out = "Hello";
         return (input + out + "There");
     }
+    public static 
 
 
 }
--- src/JudyLeung/Main.java
+++ src/JudyLeung/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(+JLeungLib.test("hi"));
+        System.out.println(JLeungLib.test("hi"));
+    }
+
+    public static boolean isPalindrome(String input){
+        String backwards = "";
+        int i = input.length();
+        while (i > 0)
+        {
+            backwards = backwards + input.substring(i-1, i);
+        }
+        return (backwards.equals(input));
     }
 }
